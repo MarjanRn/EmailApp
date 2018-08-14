5e2852b7e7fc9c3d9941e7528579bdb691939b79
mramin <ramin.marjan@gmail.com>
2018-08-14 14:21:29 -0400

    Added new package for Student database App

---
+++ src/studentdatabaseapp/Student.java
@@ -0,0 +1,32 @@
+package studentdatabaseapp;
+
+public class Student {
+
+    String firstName;
+    String lastName;
+    int studentId;
+
+
+    //Constructor: prompt user to enter student's name and year
+    Student(){
+
+
+    }
+
+    //Generate an unique student ID
+
+
+    //Enroll in courses
+
+
+    //View balance
+
+
+    //Pay tuition
+
+
+    //Show student's status
+
+
+
+}
---
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -0,0 +1,14 @@
+package studentdatabaseapp;
+
+public class StudentDatabaseApp {
+    public static void main(String[] args) {
+        //Ask how many users you want to add
+
+
+        //Create n number of new students
+
+
+        Student student = new Student();
+
+    }
+}
