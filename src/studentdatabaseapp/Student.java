c4d00892386b8d10bdfd9618a6b520142c2d79ed
mramin <ramin.marjan@gmail.com>
2018-08-18 11:33:05 -0400

    update

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -1,32 +1,82 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class Student {
 
-    String firstName;
-    String lastName;
-    int studentId;
+    private String firstName;
+    private String lastName;
+    private String studentID;
+    private int gradeYear;
+    private String courses;
+    private int tuitionBalance = 0;
+    private static int costOfCourse = 600;
+    private static int id = 1000;
 
 
     //Constructor: prompt user to enter student's name and year
-    Student(){
+    public Student(){
+        System.out.println("Please enter the student's first name: ");
+        Scanner firstNameIn = new Scanner(System.in);
+        this.firstName = firstNameIn.nextLine();
 
+        System.out.println("Please enter the student's last name: ");
+        Scanner lastNameIn = new Scanner(System.in);
+        this.lastName = lastNameIn.nextLine();
 
+        System.out.println("Please enter the student's level: " +
+                "\n1 for First Year \n2 for Second Year \n3 for Third Year \n4 for Last Year");
+        Scanner levelIn = new Scanner(System.in);
+        this.gradeYear = levelIn.nextInt();
+
+        generateStudentID(gradeYear);
     }
 
     //Generate an unique student ID
-
+    private void generateStudentID(int gradeYear){
+        this.studentID = gradeYear + "" + id++;
+    }
 
     //Enroll in courses
+    public void courseEnrolement(){
 
+        do {
+            System.out.println("Enter the course name to enrol(Q to exit)");
+            Scanner in = new Scanner(System.in);
+            String course = in.nextLine();
+            if (!course.equals("Q")) {
+                courses =  courses + "\n" + course;
+                tuitionBalance = tuitionBalance + costOfCourse;
+            }
+            else {
+                break;
+            }
+        }while (1 == 1);
+
+    }
 
     //View balance
-
+    public void viewBalance(){
+        System.out.println("Your balance is: " + tuitionBalance);
+    }
 
     //Pay tuition
+    public void payTuition(){
+        viewBalance();
+        System.out.println("Enter your payment amount: ");
+        Scanner in = new Scanner(System.in);
+        int payment = in.nextInt();
+        tuitionBalance = tuitionBalance - payment;
+        System.out.println("Thank you for your payment of $" + payment);
+        viewBalance();
+    }
 
 
     //Show student's status
+    public void showStudentsStatus(){
 
+        System.out.println(studentID + firstName + lastName + gradeYear + courses + tuitionBalance );
+    }
 
 
 }
--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -1,14 +1,24 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
     public static void main(String[] args) {
-        //Ask how many users you want to add
+        //Ask how many students you want to add
+        System.out.println("Enter number of students to enrol:");
+        Scanner in = new Scanner(System.in);
+        int numOfStudents = in.nextInt();
+        Student[] students = new Student[numOfStudents];
+
+
 
 
         //Create n number of new students
 
 
         Student student = new Student();
-
+        student.courseEnrolement();
+        student.payTuition();
+        student.showStudentsStatus();
     }
 }
